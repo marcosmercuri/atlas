ef8ae5df192360549879787b35c8ce999bc1b295
Marcos Mercuri <marcosmercuri@packlink.es>
2015-11-11 22:26:16 +0100

    first commit

---
+++ src/main/java/com/crossfit/model/Exercise.java
@@ -0,0 +1,10 @@
+package com.crossfit.model;
+
+import com.google.common.collect.Range;
+
+/**
+ * Represents a possible physical exercise to be done.
+ */
+public class Exercise {
+    private Range<>
+}
---
+++ src/main/java/com/crossfit/model/Task.java
@@ -0,0 +1,8 @@
+package com.crossfit.model;
+
+/**
+ * Represents a physical exercise with some kind of a counter
+ */
+public class Task {
+    private Exercise exercise;
+}
