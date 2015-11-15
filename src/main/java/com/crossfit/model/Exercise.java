4652f5d0f35ea8d106c6b9907e171a15d3f5397e
Marcos Mercuri <marcosmercuri@packlink.es>
2015-11-15 20:44:45 +0100

    model
    merge conflict

--- src/main/java/com/crossfit/model/Exercise.java
+++ src/main/java/com/crossfit/model/Exercise.java
@@ -1,16 +1,40 @@
 package com.crossfit.model;
 
-import com.google.common.collect.Range;
-
 /**
  * Represents a possible physical exercise to be done.
  */
 public class Exercise {
-    private Range<Integer> maleRangeWeight;
-    private Range<Integer> femaleRangeWeight;
+    private Integer maleRxInKilograms;
+    private Integer femaleRxInKilograms;
+    private String name;
+    private String description;
 
-    public Exercise (Range<Integer> maleRangeWeight, Range<Integer> femaleRangeWeight) {
-        this.maleRangeWeight = maleRangeWeight;
-        this.femaleRangeWeight = femaleRangeWeight;
+    public Exercise (Integer maleRxInKilograms, Integer femaleRxInKilograms, String name, String description) {
+        this.maleRxInKilograms = maleRxInKilograms;
+        this.femaleRxInKilograms = femaleRxInKilograms;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Exercise (Integer maleRxInKilograms, Integer femaleRxInKilograms, String name) {
+        this.maleRxInKilograms = maleRxInKilograms;
+        this.femaleRxInKilograms = femaleRxInKilograms;
+        this.name = name;
+    }
+
+    public Integer getMaleRxInKilograms () {
+        return maleRxInKilograms;
+    }
+
+    public Integer getFemaleRxInKilograms () {
+        return femaleRxInKilograms;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public String getDescription () {
+        return description;
     }
 }
