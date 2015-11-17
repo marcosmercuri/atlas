d0dc46b20b297b222a7506ed5fcd3ed7d20aefd7
Irina <Irina>
2015-11-17 21:06:20 +0100

    model
    Add javadoc

--- src/main/java/com/crossfit/model/Amrap.java
+++ src/main/java/com/crossfit/model/Amrap.java
@@ -3,7 +3,8 @@ package com.crossfit.model;
 import java.util.List;
 
 /**
- *
+ * A type of workout in which a list of exercises must be repeated
+ * for durationInMinutes minutes.
  */
 public class Amrap extends Workout {
     private Integer durationInMinutes;
