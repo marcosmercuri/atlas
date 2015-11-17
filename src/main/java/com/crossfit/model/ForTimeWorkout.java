9bcfb2b4a257ce8c012e260d2321cae2b15ff393
Irina <Irina>
2015-11-17 21:06:43 +0100

    model
    Add number of rounds property in workout

--- src/main/java/com/crossfit/model/ForTimeWorkout.java
+++ src/main/java/com/crossfit/model/ForTimeWorkout.java
@@ -3,17 +3,24 @@ package com.crossfit.model;
 import java.util.List;
 
 /**
- *
+ * A type of workout in which a list of exercises must be repeated
+ * numberOfRounds times, in a max time of maxAllowedMinutes minutes.
  */
 public class ForTimeWorkout extends Workout {
     private Integer maxAllowedMinutes;
+    private Integer numberOfRounds;
 
-    public ForTimeWorkout (List<Exercise> exercises, Integer maxAllowedMinutes) {
+    public ForTimeWorkout (List<Exercise> exercises, Integer maxAllowedMinutes, Integer numberOfRounds) {
         super(exercises);
         this.maxAllowedMinutes = maxAllowedMinutes;
+        this.numberOfRounds = numberOfRounds;
     }
 
     public Integer getMaxAllowedMinutes () {
         return maxAllowedMinutes;
+    }
+
+    public Integer getNumberOfRounds () {
+        return numberOfRounds;
     }
 }
