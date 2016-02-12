dc3cdd28cd2a1ebf2a286b46848c57c1855152a6
Marcos Mercuri <marcosmercuri@packlink.es>
2016-02-12 20:55:32 +0100

    refactor
    Make the exception package visible

--- src/main/java/com/crossfit/util/UnableToOpenResourceException.java
+++ src/main/java/com/crossfit/util/UnableToOpenResourceException.java
@@ -1,6 +1,7 @@
 package com.crossfit.util;
 
-public class UnableToOpenResourceException extends RuntimeException {
+class UnableToOpenResourceException extends RuntimeException {
+
     public UnableToOpenResourceException (String fileName, Exception originalException) {
         super(String.format("Unable to open the file %s", fileName), originalException);
     }
