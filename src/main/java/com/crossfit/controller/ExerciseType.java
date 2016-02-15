686e4d0b9eddf7c87f54118f3e82c9a17fb34941
Marcos Mercuri <marcosmercuri@packlink.es>
2016-02-15 23:43:12 +0100

    Doc
    Add javadoc

--- src/main/java/com/crossfit/controller/ExerciseType.java
+++ src/main/java/com/crossfit/controller/ExerciseType.java
@@ -1,5 +1,20 @@
 package com.crossfit.controller;
 
+/**
+ * Represents the different types of exercise, ie, if it's an exercise that has to be repeated a number
+ * of times, or if it has to be done for a an amount of time, etc.
+ */
 enum ExerciseType {
-    REPETITION, TIMED, DISTANCE
+    /**
+     * The exercise has to be repeated N times.
+     */
+    REPETITION,
+    /**
+     * The exercise has to be done for a certain amount of time.
+     */
+    TIMED,
+    /**
+     * The exercise has to be done through a certain distance, such as running.
+     */
+    DISTANCE
 }
