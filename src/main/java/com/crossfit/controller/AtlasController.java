279f194dd2d04c524ff9bb74b1b35f9540bb5007
Marcos Mercuri <marcosmercuri@packlink.es>
2016-03-13 12:36:04 +0100

    Feature
    Call the ProposedWorkoutService on the rest client.

--- src/main/java/com/crossfit/controller/AtlasController.java
+++ src/main/java/com/crossfit/controller/AtlasController.java
@@ -5,7 +5,9 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 import java.util.UUID;
 import javax.validation.Valid;
 
+import com.crossfit.mappers.ProposedWorkoutMapper;
 import com.crossfit.model.Exercise;
+import com.crossfit.services.ProposedWorkoutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.WebDataBinder;
@@ -20,6 +22,9 @@ public class AtlasController {
     @Autowired
     private ProposedWorkoutValidator proposedWorkoutValidator;
 
+    @Autowired
+    private ProposedWorkoutService proposedWorkoutService;
+
     @InitBinder
     protected void initBinder(WebDataBinder binder) {
         binder.addValidators(proposedWorkoutValidator);
@@ -28,10 +33,7 @@ public class AtlasController {
     @RequestMapping (value = "/proposedWorkouts", method = POST, produces = "application/json; charset=utf-8")
     @ResponseStatus(HttpStatus.CREATED)
     public ProposedWorkoutDTO createProposedWorkout(@Valid @RequestBody ProposedWorkoutDTO proposedWorkout) {
-        //saveProposedWorkout = proposedWorkoutManager.saveProposedWorkout(mapper.mapToProposedWorkout(proposedWorkout));
-        //return mapper.mapToProposedWorkoutDTO(savedProposedWorkout);
-        proposedWorkout.setId(UUID.randomUUID().toString());
-        return proposedWorkout;
+        return proposedWorkoutService.saveProposedWorkout(proposedWorkout);
     }
 
     @RequestMapping (value = "/proposedWorkouts/{id}/exercises", method = POST)
