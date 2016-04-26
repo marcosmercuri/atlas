33c3cc11845f910b18b60dd3e0abae182a46abc6
Marcos Mercuri <marcosmercuri@gmail.com>
2016-04-26 23:59:18 +0200

    Housekeeping
    Remove TODO, as the name of the repository is correct.

--- src/main/java/com/crossfit/repositories/ProposedWorkoutRepository.java
+++ src/main/java/com/crossfit/repositories/ProposedWorkoutRepository.java
@@ -3,6 +3,5 @@ package com.crossfit.repositories;
 import com.crossfit.model.Workout;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-//TODO See if it has to be renamed to workoutRepository
 public interface ProposedWorkoutRepository extends MongoRepository<Workout, String> {
 }
