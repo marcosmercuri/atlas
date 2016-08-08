c7fba511fde7340d892e652775d2137b8b238c13
Marcos Mercuri <marcosmercuri@gmail.com>
2016-08-09 00:03:16 +0200

    Test
    Add exclusion of some beans to disable the security
    in the integration tests.

--- src/test/java/com/crossfit/AbstractIntegrationTest.java
+++ src/test/java/com/crossfit/AbstractIntegrationTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 
 /**
  * This class loads the entire application, so it can be used for real end-to-end test.
@@ -22,6 +23,8 @@ import org.springframework.http.MediaType;
  */
 @SpringApplicationConfiguration (classes = AtlasApplication.class)
 @WebIntegrationTest ("server.port:0")
+// Profile activated to disable spring security features.
+@ActiveProfiles("test")
 public class AbstractIntegrationTest {
     //In this way I can get the port dynamically without hardcoding it
     @Value ("${local.server.port}")
