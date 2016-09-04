dddf31a3fec056b95ca6b3bd8bba7ca230af62e3
Marcos Mercuri <marcosmercuri@gmail.com>
2016-09-04 20:30:46 +0200

    Feature: Return userProfile
    After the user is logged in into session, the userProfile
    is returned.

--- src/main/java/com/crossfit/security/AuthUtil.java
+++ src/main/java/com/crossfit/security/AuthUtil.java
@@ -13,12 +13,14 @@ class AuthUtil {
     /**
      * Takes a Social Connection and authenticate the user using Spring Security's context.
      * @param connection connection with the user's info
+     * @return the logged in userProfile.
      */
-    static void authenticate(Connection<?> connection) {
+    static UserProfile authenticate(Connection<?> connection) {
         UserProfile userProfile = connection.fetchUserProfile();
         String username = userProfile.getUsername();
         UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null, null);
         SecurityContextHolder.getContext().setAuthentication(authentication);
         LOGGER.info("User {} {} connected.", userProfile.getFirstName(), userProfile.getLastName());
+        return userProfile;
     }
 }
